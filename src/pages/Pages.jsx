5ba4deda6f02e79cd2c10be7c145757395cb0c49
hdrock03 <hardik.1rn16ec036@gmail.com>
2022-09-19 18:52:49 +0530

    completed

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Home from "./Home";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Cuisine from "./Cuisine";
 import Searched from "./Searched";
 import Recipe from "./Recipe";
 import { AnimatePresence } from "framer-motion";
 
 function Pages() {
+  const location = useLocation()
   return (
     //{/* browserRouter phle pages.jsx me wrapped tha jisse error aa rha tha useLocation() may be used only in the context of <Router> component</Router> */}
     // to usko app.jsx me wrap kiye
     <AnimatePresence >
-      <Routes>
+      <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Home />} />
         <Route path="/cuisine/:type" element={<Cuisine />} />
         <Route path="/searched/:search" element={<Searched />} />
